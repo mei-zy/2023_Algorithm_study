9a6cedbe11721e074493900c6de5920342f3e837
urther <alswlkku@ajou.ac.kr>
2023-04-02 10:10:20 +0900

    3번 문제 풀이

--- 해몽모음/03.js
+++ 해몽모음/03.js
@@ -1,4 +1,27 @@
-const solution = () => {};
+const solution = (arr) => {
+  let answer = 0;
+  let count = [0];
+  let map = new Map();
+
+  arr.unshift(0);
+
+  for (let i = 1; i < arr.length; i++) {
+    const x = arr[i] + count[i - 1];
+    count.push(x);
+  }
+
+  for (let i = 0; i < count.length; i++) {
+    const x = map.get(count[i]);
+
+    if (x) {
+      answer += x;
+    }
+
+    map.set(count[i], x ? x + 1 : 1);
+  }
+
+  return answer;
+};
 
 console.log(solution([5, -5, 6, 0, 2, -8])); //4
-console.log(solution([-5, 3, 0, -3, 10])); //4
+console.log(solution([-5, 3, 0, -3, 10])); //2
