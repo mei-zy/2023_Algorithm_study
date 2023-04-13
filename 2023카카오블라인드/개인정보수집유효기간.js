1ea835105e8c658281ad7ce1490459c2139af545
urther <alswlkku@ajou.ac.kr>
2023-04-14 00:30:48 +0900

    프로그래머스 개인정보수집유효기간

--- 2023카카오블라인드/개인정보수집유효기간.js
+++ 2023카카오블라인드/개인정보수집유효기간.js
@@ -1,60 +1,41 @@
-const months = {
-  1: 31,
-  2: 28,
-  3: 31,
-  4: 30,
-  5: 31,
-  6: 30,
-  7: 31,
-  8: 31,
-  9: 30,
-  10: 31,
-  11: 30,
-  12: 31,
-};
+const MAX = 28;
 
-const isExceedDate = (base, compare) => {
-  const [byear, bmonth, bdate] = base.split(".").map(Number);
-  const [cyear, cmonth, cdate] = compare.split(".").map(Number);
+// true면 파기
+const checkDestruction = (base, compare) => {
+  const [by, bm, bd] = base.split(".").map(Number);
+  const [cy, cm, cd] = compare.split(".").map(Number);
 
-  if (byear > cyear) return false;
-  else if (byear < cyear) return true;
-  else {
-    if (bmonth < cmonth) return true;
-    else if (bmonth > cmonth) return false;
-    else {
-      if (cdate >= bdate) return true;
-      else false;
-    }
-  }
-};
-
-const calValidity = (base, addMonth) => {
-  let [year, month, date] = base.split(".").map(Number);
-
-  month += addMonth;
-
-  if (month > 12) {
-    const x = Math.round(addMonth / 12);
-    year += x;
-    month -= 12 * x;
-
-    console.log("year", year, "month", month, "x", x);
+  if (by < cy) return false;
+  else if (by === cy) {
+    if (cm === bm) {
+      // date 확인
+      if (bd <= cd) return false;
+    } else if (bm < cm) return false;
   }
 
-  // date 줄이기
+  return true;
+};
+
+const calDestruction = (dateStr, ex) => {
+  let [year, month, date] = dateStr.split(".").map(Number);
+
+  month += ex;
   date -= 1;
 
+  // 달을 빼준다
   if (date === 0) {
-    if (month === 1) {
-      const d = months[12];
-      date = d;
-      month = 12;
-      year--;
-    } else {
-      const d = months[month--];
-      date = d;
-    }
+    month -= 1;
+    date = MAX;
+  }
+
+  if (month > 12) {
+    const add = Math.floor(month / 12);
+    month -= add * 12;
+    year += add;
+  }
+  if (month === 0) {
+    month = 12;
+    year -= 1;
   }
 
   return `${year}.${month}.${date}`;
@@ -62,24 +43,43 @@ const calValidity = (base, addMonth) => {
 
 function solution(today, terms, privacies) {
   var answer = [];
-
-  const termChart = new Map();
+  const des = new Map();
 
   for (let term of terms) {
-    const [kind, date] = term.split(" ");
-    termChart.set(kind, date);
+    const [alpha, ex] = term.split(" ");
+    des.set(alpha, +ex);
   }
 
-  let i = 1;
+  let index = 1;
 
   for (let privacie of privacies) {
-    const [days, alpha] = privacie.split(" ");
-    const date = calValidity(days, +termChart.get(alpha));
-    console.log(date);
-
-    if (isExceedDate(today, date)) answer.push(i);
-    i++;
+    const [date, alpha] = privacie.split(" ");
+    const base = calDestruction(date, des.get(alpha));
+    if (checkDestruction(today, base)) answer.push(index);
+    index++;
   }
 
   return answer;
 }
+
+// console.log(
+//   solution(
+//     "2022.05.19",
+//     ["A 6", "B 12", "C 3"],
+//     ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]
+//   )
+// );
+
+console.log(
+  solution(
+    "2020.01.01",
+    ["Z 3", "D 5"],
+    [
+      "2019.01.01 D",
+      "2019.11.15 Z",
+      "2019.08.02 D",
+      "2019.07.01 D",
+      "2018.12.28 Z",
+    ]
+  )
+);
